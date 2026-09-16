44da65ce167b52e3cf13bc526dd398627c737e4e
Juan Camilo Leal <juancamiloleal07@gmail.com>
2026-09-16 15:21:18 -0500

    feat(firebase): add support for optional measurementId

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || '',
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '',
   appId: import.meta.env.VITE_FIREBASE_APP_ID || '',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || '',
 };
 
 // Check if credentials have been customized or if demo defaults are present
